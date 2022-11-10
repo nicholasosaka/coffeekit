e94f7a923aa5c669f41370b2dd410e9524d50901
Nicholas Osaka <nicholas.osaka@gmail.com>
2022-11-09 20:23:00 -0500

    Initial Commit

---
+++ coffeekit/coffeekitApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeekitApp.swift
+//  coffeekit
+//
+//  Created by Nicholas Osaka on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct coffeekitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
